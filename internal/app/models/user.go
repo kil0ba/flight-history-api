df09631c811a6c804a5cbdcbae93ce35d85cfe19
David <gurshumov.david@gmail.com>
2024-04-03 21:01:50 +0300

    add unit tests

--- internal/app/models/user.go
+++ internal/app/models/user.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"github.com/go-playground/validator/v10"
+	"github.com/kil0ba/flight-history-api/internal/app/models/validators"
 )
 
 type User struct {
@@ -9,11 +10,14 @@ type User struct {
 	Email             string `validate:"required,email"`
 	Password          string `validate:"required,min=6"`
 	EncryptedPassword string
-	Login             string `validate:"required,min=6,alphanum"`
+	Login             string `validate:"required,min=6,username"`
 }
 
 func (u *User) Validate() error {
 	validate := validator.New()
+	// Register a custom validation function for the "usernameformat" tag
+	validate.RegisterValidation("username", validators.Username)
+
 	err := validate.Struct(u)
 
 	if err != nil {
